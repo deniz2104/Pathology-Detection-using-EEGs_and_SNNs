4441e3de3afaa55bf9fcb469701ad9bbf3da93cf
deniz2104 <sined2101@gmail.com>
2025-12-09 16:13:57 +0200

    updated pipeline preprocessing

--- eeg_preprocessing_pipeline.py
+++ eeg_preprocessing_pipeline.py
@@ -11,6 +11,7 @@ from collect_eeg_data_for_each_subject import collect_eeg_files
 from gather_list_of_subjects import get_list_of_subjects
 from get_google_drive_service import get_google_drive_service
 import constants
+from pyprep.find_noisy_channels import NoisyChannels
 
 class EEGPreprocessingPipeline:
     def __init__(self, subject_folder_id):
@@ -45,27 +46,25 @@ class EEGPreprocessingPipeline:
         return eeg_files
 
     def get_raw_eeg_from_a_subject(self, file_id, filename):
-        service = get_google_drive_service()
+        service = get_google_drive_service() 
         
         try:
             request = service.files().get_media(fileId=file_id)
-            fh = BytesIO()
-            downloader = MediaIoBaseDownload(fh, request)
-            done = False
             
             print(f"Streaming {filename} from Google Drive...")
-            while not done:
-                status, done = downloader.next_chunk()
-                if status:
-                    print(f"  Downloaded {int(status.progress() * 100)}%")
             
-            fh.seek(0)
-            
-            print(f"Reading EEG data from {filename}...")
             with tempfile.NamedTemporaryFile(suffix='.set', delete=False) as tmp:
-                tmp.write(fh.getvalue())
+                downloader = MediaIoBaseDownload(tmp, request)
+                
+                done = False
+                while not done:
+                    status, done = downloader.next_chunk()
+                    if status:
+                        print(f"  Downloaded {int(status.progress() * 100)}%")
+                
                 tmp_path = tmp.name
-            
+
+            print(f"Reading EEG data from {filename}...")
             try:
                 raw = mne.io.read_raw_eeglab(tmp_path, preload=True)
                 return raw
@@ -135,14 +134,10 @@ class EEGPreprocessingPipeline:
         print("Internal first sample index:", eeg_raw.first_samp)
         print("Internal last sample index:", eeg_raw.last_samp)
 
-    def identify_dead_channels(self, eeg_raw):
-        list_of_dead_channels = []
-        for ch in eeg_raw.ch_names:
-            data, _ = eeg_raw[ch]
-            peak_to_peak = np.ptp(data)
-            if peak_to_peak <= 1e-7:
-                list_of_dead_channels.append(ch)
-        return list_of_dead_channels
+    def make_montage(self, eeg_raw):
+        montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
+        eeg_raw.set_montage(montage, match_case=False)
+        return eeg_raw
 
     def print_channel_statistics(self, eeg_raw):
         data = eeg_raw.get_data()
@@ -154,12 +149,6 @@ class EEGPreprocessingPipeline:
         print(f"Dead (Flat) Channels: {len(dead_channels)}")
         print(f"Living (Active) Channels: {len(living_channels)}")
 
-        list_of_dead_channels = self.identify_dead_channels(eeg_raw)
-        if list_of_dead_channels:
-            print("\nSummary of Dead Channels:")
-            for dead_ch in list_of_dead_channels:
-                print(f" - {dead_ch}")
-
     def print_eeg_summary(self, eeg_raw):
         self.print_filter_info(eeg_raw)
         self.print_channel_info(eeg_raw)
@@ -169,6 +158,34 @@ class EEGPreprocessingPipeline:
         self.print_annotations(eeg_raw)
         self.print_channel_statistics(eeg_raw)
 
+    def plot_power_spectral_density(self, eeg_raw, duration=4.0):
+        sfreq = eeg_raw.info['sfreq']
+        n_fft = int(sfreq * duration)
+
+        try:
+            spectrum = eeg_raw.compute_psd(n_fft=n_fft)
+            spectrum.plot()
+        except AttributeError:
+            eeg_raw.plot_psd(n_fft=n_fft)
+
+    def sanitize_channel_names(self, eeg_raw):
+        if eeg_raw.info['bads']:
+            eeg_raw.info['bads'] = [str(ch) for ch in eeg_raw.info['bads']]
+            
+        return eeg_raw
+    
+    def set_eeg_reference(self, eeg_raw):
+        eeg_raw.set_eeg_reference('average', projection=False)
+        return eeg_raw
+
+    def interpolate_bad_channels(self, eeg_raw):
+        eeg_raw.interpolate_bads(reset_bads=True,method='spline')
+        return eeg_raw
+    
+    def apply_notch_filter(self, eeg_raw):
+        eeg_raw.notch_filter(freqs=[60.0,120.0,180.0], picks = 'eeg', method='fir', filter_length='auto', phase='zero')
+        return eeg_raw
+
     def plot_sensor_locations(self, eeg_raw):
         fig = eeg_raw.plot_sensors(kind='3d', show_names=True, title="GSN 129 Sensors")
         fig.savefig("eeg_plots/gsn_129_sensors_3d.png", dpi=300, bbox_inches='tight')
@@ -176,7 +193,53 @@ class EEGPreprocessingPipeline:
     def plot_sensor_topomap(self, eeg_raw):
         fig = eeg_raw.plot_sensors(kind='topomap', show_names=True, title="GSN 129 Topomap")
         fig.savefig("eeg_plots/gsn_129_topomap.png", dpi=300, bbox_inches='tight')
+    
+    def select_important_frequency_bands(self, eeg_raw):
+        bands = {
+            'Delta': (0.5, 4),
+            'Theta': (4, 8),
+            'Alpha': (8, 12),
+            'Beta': (12, 30),
+            'Gamma': (30, 100)
+            }
+        
+        eeg_data = {}
+        for band_name, (fmin, fmax) in bands.items():
+            filtered = eeg_raw.copy().filter(l_freq=fmin, h_freq=fmax)
+            eeg_data[band_name] = filtered
+        
+        return eeg_data
+    
+    def fix_scaling_units(self, eeg_raw):
+        data = eeg_raw.get_data()
+        
+        max_val = np.max(np.abs(data))
+        
+        if max_val > 1.0: 
+            print(f"DETECTED SCALING ERROR: Values are too large for Volts.")
+            print(f"Likely in microvolts (uV). Scaling by 1e-6...")
+            eeg_raw.apply_function(lambda x: x * 1e-6)
+            print(f"   ✅ New Max: {np.max(np.abs(eeg_raw.get_data())):.5e} V")
+            
+        elif max_val > 0.01 and max_val <= 1.0:
+            print(f"DETECTED SCALING ERROR: Values look like millivolts (mV).")
+            print(f"Scaling by 1e-3...")
+            eeg_raw.apply_function(lambda x: x * 1e-3)
+            print(f"   ✅ New Max: {np.max(np.abs(eeg_raw.get_data())):.5e} V")
+            
+        else:
+            print("✅ Scaling looks correct (Standard EEG amplitude range).")
+            
+        return eeg_raw
+    
+    def detect_bads_pyprep(self, eeg_raw):
+        nd = NoisyChannels(eeg_raw)
+        nd.find_all_bads(ransac=True)
+        eeg_raw.info['bads'] = nd.get_bads()
+        if 'Cz' in eeg_raw.info['bads']:
+            eeg_raw.info['bads'].remove('Cz')
 
+        return eeg_raw
 
 def main():
     subjects = get_list_of_subjects()
@@ -184,7 +247,18 @@ def main():
         pipeline = EEGPreprocessingPipeline(subject)
         
         raw_data = pipeline.process_raw_eeg_data()
-    
+        for eeg_entry in raw_data:
+            eeg_raw = eeg_entry['raw']
+            print(f"\n=== Summary for {eeg_entry['filename']} ===")            
+            eeg_raw = pipeline.fix_scaling_units(eeg_raw)
+            eeg_raw = pipeline.make_montage(eeg_raw)
+            eeg_raw = pipeline.detect_bads_pyprep(eeg_raw)
+            eeg_raw = pipeline.apply_notch_filter(eeg_raw)
+            eeg_raw = pipeline.sanitize_channel_names(eeg_raw)
+            eeg_raw = pipeline.set_eeg_reference(eeg_raw)
+            eeg_raw = pipeline.interpolate_bad_channels(eeg_raw)
+            pipeline.plot_power_spectral_density(eeg_raw)
+
     plt.show()
 
 if __name__ == "__main__":
